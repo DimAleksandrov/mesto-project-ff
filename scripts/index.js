9368e88585d8c297ccf9e992622f12352a172b15
Dima Aleksandrov <aleksandrovdmitry.bk@yandex.ru>
2024-05-02 14:10:07 +0300

    Первая отправка на проверку

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addCard(elem, deleteCard) {
+    const placesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const deleteButton = placesItem.querySelector('.card__delete-button');
+
+    placesItem.querySelector('.card__image').src = elem.link;
+    placesItem.querySelector('.card__title').textContent = elem.name;
+
+    placesList.append(placesItem);
+
+    deleteButton.addEventListener('click', deleteCard);
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(event) {
+    const listItem = event.target.closest('.places__item');
+    listItem.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (elem) {
+    addCard(elem, deleteCard);
+})
