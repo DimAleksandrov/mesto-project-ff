3d525cb51f7e899d59571a33906393662efe655e
Dima Aleksandrov <aleksandrovdmitry.bk@yandex.ru>
2024-06-08 09:31:18 +0300

    ошибка высвечивается кнопка disabled при редактировании профиля

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const popupTypeEdit = document.querySelector('.popup_type_edit');
 const popupTypeNewCard = document.querySelector('.popup_type_new-card');
 const popupTypeImage = document.querySelector('.popup_type_image');
 const closePopupButtons = document.querySelectorAll('.popup__close');
-const popupButton = document.querySelector('.popup__button');
+// const popupButton = document.querySelector('.popup__button');
 const formElement = document.forms.edit_profile;
 const formElementNewPlace = document.forms.new_place;
 const nameInput = formElement.elements.name;
@@ -25,6 +25,87 @@ const profileTitle = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__description');
 const popupImage = document.querySelector('.popup__image');
 const popupCaption = document.querySelector('.popup__caption');
+
+
+
+
+// -----------------------------------------------------------------------------
+const showInputError = (formElement, inputElement, errorMessage) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add('popup__input_type_error');
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('popup__input-error_active');
+  };
+  
+  const hideInputError = (formElement, inputElement) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove('popup__input_type_error');
+    errorElement.classList.remove('popup__input-error_active');
+    errorElement.textContent = '';
+  };
+  
+  const hasInvalidInput = (inputList) => {
+    return inputList.some((inputElement) => {
+       return !inputElement.validity.valid;
+    });
+  };
+  
+  const checkInputValidity = (formElement, inputElement) => {
+    if (!inputElement.validity.valid) {
+      showInputError(formElement, inputElement, inputElement.validationMessage);
+    } else {
+      hideInputError(formElement, inputElement);
+    }
+  };
+  
+  const toggleButtonState = (inputList, buttonElement) => {
+    if (hasInvalidInput(inputList)) {
+      buttonElement.disablesd = true;
+      buttonElement.classList.add('button_inactive');
+    } else {
+      buttonElement.classList.remove('button_inactive');
+    }
+  }; 
+  
+  const setEventListeners = (formElement) => {
+    const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+    const buttonElement = formElement.querySelector('.popup__button');
+    toggleButtonState(inputList, buttonElement);
+    inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', function () {
+        checkInputValidity(formElement, inputElement);
+        toggleButtonState(inputList, buttonElement);
+      });
+    });
+  };
+  
+//   const enableValidation = () => {
+//     const formList = Array.from(document.querySelectorAll('.popup__form'));
+//     formList.forEach((formElement) => {
+//       formElement.addEventListener('submit', function (evt) {
+//         evt.preventDefault();
+//         setEventListeners(formElement);
+//       });
+
+      
+  
+//     //   const fieldsetList = Array.from(formElement.querySelectorAll('.form__set'));
+      
+//     //   fieldsetList.forEach((fieldSet) => {
+//     //     setEventListeners(fieldSet);
+//     //   });
+//     });
+//   };
+  
+// enableValidation();
+// -----------------------------------------------------------------------------
+
+
+
+// enableValidation();
+
+
+
 
 let popup = '';
 
@@ -99,12 +180,13 @@ function setInitialValues() {
   
   
 function handleFormSubmitProfil(evt) {
-    evt.preventDefault(); 
+    evt.preventDefault();
     profileTitle.textContent = nameInput.value;
     profileDescription.textContent = jobInput.value;
     closePopup(popupTypeEdit);
 };
 
+setEventListeners(popupTypeEdit);
 formElement.addEventListener('submit', handleFormSubmitProfil);
 
 function handleFormSubmitPlace(evt) {
